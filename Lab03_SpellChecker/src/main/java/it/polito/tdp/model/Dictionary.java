5c3a972a564a963ef2abe71bba0589a091f40ea8
Alessandro Sandiano <Alessandro Sandiano@pc-Alex.station>
2020-06-26 01:11:58 +0200

    miglioramento gestione menu a tendina

--- Lab03_SpellChecker/src/main/java/it/polito/tdp/model/Dictionary.java
+++ Lab03_SpellChecker/src/main/java/it/polito/tdp/model/Dictionary.java
@@ -8,6 +8,8 @@ public class Dictionary {
 	Set<String> dictionaryWords = new HashSet<>();
 	
 	public void loadDictionary (String language) {
+		if (dictionaryWords.size() > 0)
+			return;
 		try {
 			FileReader fr = new FileReader("src/main/resources/" + language + ".txt");
 			BufferedReader br = new BufferedReader(fr);
